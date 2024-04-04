19a63a9d3e1a73937a75d0f5f407076071f9d191
FBAgol <farzadgolzari1999@gmail.com>
2024-04-04 11:28:32 +0200

    added more test data

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from Observer import Observerable, Person
 
 if __name__== "__main__":
     observer=Observerable()
-    person1= Person(['Ales', 'A'])
+    person1= Person(['Alex', 'A'])
+    person2= Person(['Farzad', 'D'])
+    person3= Person(['Michael', 'C'])
+
     observer.addObserver(person1)
-    observer.addFrucht(["aplle" , "B"])
+    observer.addObserver(person2)
+    observer.addObserver(person3)
+
+    observer.addFrucht(["Apfel" , "B"])
+    observer.addFrucht(["Banana" , "A"])
+    observer.addFrucht(["Kivi" , "A"])
 
    
